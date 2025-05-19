1e3d4536ffe2e8d21d13ef0596090c72cd7bf00a
Jacques Sovoessi <jacques.sovoessi@gmail.com>
2025-05-19 14:31:40 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>  
+    </>
+  )
+}
+
+export default App
